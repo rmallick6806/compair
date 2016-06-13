799d782becbf7d6d1c6807d553826f029861886d
Ray Malllick <rmallick@triadinteractive.com>
2016-06-13 12:35:43 -0400

    clean up

--- js/init.js
+++ js/init.js
@@ -1,4 +1,13 @@
 var init = function() {
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAZF-kcVUR4l8B-IRGgxMxHUO870hpHRtc",
+    authDomain: "compair-60214.firebaseapp.com",
+    databaseURL: "https://compair-60214.firebaseio.com",
+    storageBucket: "compair-60214.appspot.com",
+  };
+  firebase.initializeApp(config);
+
   Array.prototype.randomElement = function() {
     return this[Math.floor(Math.random() * this.length)]
   }
